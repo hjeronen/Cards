99374b6e8a3713ab9d03a1d954ad4c626dca4de3
Heli Eronen <herojonen@gmail.com>
2023-10-28 18:36:59 +0300

    Configure JavaFX

--- Cards/app/build.gradle.kts
+++ Cards/app/build.gradle.kts
@@ -8,6 +8,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 repositories {
@@ -28,6 +29,11 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
+}
+
+javafx {
+    version = "21.0.1"
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.graphics", "javafx.media", "javafx.web")
 }
 
 application {
